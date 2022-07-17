2eebcfb340950caa680e40d76abdd4abfa8a97cc
JorgeConfessor <jorgeconfessor@yahoo.com>
2022-07-17 16:20:34 -0300

    feat: Added list sorting in Console.java

--- src/main/java/br/ufrn/imd/file/Console.java
+++ src/main/java/br/ufrn/imd/file/Console.java
@@ -2,13 +2,16 @@ package br.ufrn.imd.file;
 
 import br.ufrn.imd.vehicle.EscortVehicle;
 
-import java.util.Map;
+import javax.sound.midi.Soundbank;
+import java.util.*;
 
 public class Console {
-    public void PrintMap(Map m){
-        for(Object id: m.keySet()){
-            EscortVehicle ev = (EscortVehicle) m.get(id);
+    public void PrintMap(Map m) {
 
+        List <EscortVehicle> SortedList = new ArrayList<>(m.values());
+        SortedList.sort((a, b) -> Integer.compare(a.getFleetNumber(), b.getFleetNumber()));
+
+        for(EscortVehicle ev: SortedList){
             System.out.println("/// " + ev.getFleetNumber() + " - " + ev.getLicensePlate() + " ///");
             System.out.println(String.format("LITERS: %,.2f" , ev.getLiters()));
             System.out.println(String.format("AVG LT COST: %,.2f", ev.getAverageLtCost()));
