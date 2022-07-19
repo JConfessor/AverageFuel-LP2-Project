4ea9c59e40fc74ed979e374300ab55ef8dae6af9
JorgeConfessor <jorgeconfessor@yahoo.com>
2022-07-18 22:07:40 -0300

    feat: Add fuction jtable

--- src/main/java/br/ufrn/imd/App.java
+++ src/main/java/br/ufrn/imd/App.java
@@ -1,30 +1,9 @@
 package br.ufrn.imd;
-
-// Local
-
-// Java
 import br.ufrn.imd.ui.UI;
-
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-//        JFileChooser FC = new JFileChooser();
-//        int returnVal = FC.showOpenDialog(null);
-//        if (returnVal == JFileChooser.APPROVE_OPTION) {
-//            File f = FC.getSelectedFile();
-//
-//
-//            XLSX reader = new XLSX(f.getPath());
-//            var escortvehicles = reader.readXLSX();
-//
-//
-//            Console TestConsole = new Console();
-//            TestConsole.PrintMap(escortvehicles, -1, SortTypes.FLEET, 1);
-//        }else{
-//            System.out.printf("No file was selected.");
-//        }
-
         UI gui = new UI();
     }
 }
